7e3c9a9e0d35dcac3a421a8a6122ee6c4c5d6049
Nabil Yafai <nabilyafai90@gmail.com>
2018-04-19 12:09:14 -0400

    added nunjucks

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const app = express();
-const volleyball = require('volleyball')
+const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
+
+app.set('view engine', 'html')
+app.engine('html', nunjucks.render)
+nunjucks.configure('views', {noCache: true});
+
 
 app.use((req, res, next) => {
     console.log(`Request: ${req.method} ${req.url} ${res.statusCode}`)
     next()
 })
+
 app.use('/special/',(req, res, next) => {
     console.log('You reached special zone!')
     next()
 })
+const localVariables = {
+    title : 'Welcome Page',
+    people : [
+        {name: 'Billy'},
+        {name: 'Johnny'},
+        {name: 'Non-American Name'}
+    ]
+};
 
 // app.use(volleyball)
 
-
 app.get('/', (req, res) => {
-    res.send('Welcome to our page!')
+    res.render('index', {title: localVariables.title, people: localVariables.people})
 })
 app.get('/news', (req, res) => {
     res.send('Check out the news')
